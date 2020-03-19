a8ace2a96a7e7de21aa7ba3492def02e9f994639
Kurt LaVacque <lavacquek@icloud.com>
2020-03-19 12:21:11 -0500

    Created a Sparkle Rainbow Mode

---
+++ Sparkle_Modes/Sparkle_Rainbow/Sparkle_Rainbow.ino
@@ -0,0 +1,38 @@
+#include <FastLED.h>
+#define LED_PIN     5
+#define NUM_LEDS    50
+#define BRIGHTNESS  100
+#define LED_TYPE    WS2811
+#define COLOR_ORDER RGB
+#define STROBE 1
+#define GAP 0
+#define COLOR_CHANGE_RATE 0
+CRGB leds[NUM_LEDS];
+
+//int a = 1;
+//int b = 2;
+//int c = 3;
+int h = 0;
+
+
+void setup() {
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(BRIGHTNESS);
+
+}
+
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      leds[random(NUM_LEDS)] = CHSV(random(0, 255), 255, random(0, 255)); // Change To Random LED and change color to White
+      delay(STROBE);
+      FastLED.show();
+    }
+    fill_solid(leds, NUM_LEDS, CHSV(0, 0, 0)); // Set all to Off.
+    FastLED.show();
+    delay(GAP);
+  }
+
+}
